8cc7f084e12045acd740412026300d2687b17d1e
Muhammad Younas <younas.ibms@gmail.com>
2019-03-07 00:11:46 +0500

    summernote

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
 	<title>Donation website</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="summernote/summernote.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/main.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/general.css">
 </head>
